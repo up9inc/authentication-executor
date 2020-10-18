c5f6b1626da72b4d1f33a76348033d710fc69548
Shahar Yakir <shahar.yakir@gmail.com>
2020-10-18 13:37:33 +0300

    TRA-2201 Support basic auth and header list (#4)

---
+++ authentication_executor/authenticators/headers_authenticator.py
@@ -0,0 +1,28 @@
+from authentication_executor.authenticators.base_authenticator import AuthenticationPayload, AuthenticationResult, \
+    AuthenticationStatus, AuthSpecABC, \
+    BaseAuthenticatorABC
+import base64
+
+
+class HeadersAuthenticatorSpec(AuthSpecABC):
+    def __init__(self, headers_list):
+        self.headers_list = headers_list
+
+    @staticmethod
+    def from_dict(spec: dict):
+        return HeadersAuthenticatorSpec(spec["headers"])
+
+
+class HeadersAuthenticator(BaseAuthenticatorABC):
+    spec_class = HeadersAuthenticatorSpec
+
+    def execute(self) -> AuthenticationResult:
+        headers_dict = {item['key']: item['value'] for item in self.execution_spec.headers_list}
+        return AuthenticationResult(
+            AuthenticationStatus.SUCCESS,
+            AuthenticationPayload(
+                headers=headers_dict
+            ),
+            None,
+            []
+        )
