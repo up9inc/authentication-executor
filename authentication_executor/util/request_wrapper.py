ce6c6ffa85df87b8e5b3e465897c993f933614d3
Roee Gadot <roee.gadot@up9.com>
2021-02-10 14:30:02 +0200

    no message

--- authentication_executor/util/request_wrapper.py
+++ authentication_executor/util/request_wrapper.py
@@ -1,7 +1,6 @@
 import datetime
-from urllib.parse import parse_qs, urlparse
-
 import requests
+from urllib.parse import parse_qs, urlparse
 
 
 # TODO transform cookies
@@ -28,10 +27,13 @@ class RequestWrapper:
         return [{"name": key, "value": str(val)} for key, val in headers.items()]
 
     @staticmethod
-    def _transform_body(body):
+    def _transform_body(request):
+        if request.body is None:
+            return None
+
         return {
-            "mimeType": "?",
-            "text": str(body)
+            "mimeType": request.headers.get('content-type'),
+            "text": str(request.body)
             # TODO missing params, body is interepreted as bytes
         }
 
@@ -40,29 +42,27 @@ class RequestWrapper:
         query = urlparse(request.url).query
         self._entries.append({
             "startedDateTime": (datetime.datetime.now() - resp.elapsed).astimezone().isoformat(),
-            # TODO datetime.datetime.now() - resp.elapsed,
-            "time": 0,
             "request": {
                 "method": request.method,
                 "httpVersion": '',
-                "cookies": [],  # TODO
+                "cookies": [],  # TODO list(request.cookies),
                 "headers": RequestWrapper._dict_to_array(request.headers),
                 "queryString": RequestWrapper._dict_to_array(parse_qs(query)) if query else [],
                 "headersSize": -1,  # TODO len(json.dumps(request.headers)),
-                "bodySize": -1,  # TODO len(json.dumps(request.body)) if request.body else 0,
-                "postData": RequestWrapper._transform_body(request.body) if request.body else None,
+                "bodySize": -1,     # TODO len(json.dumps(request.body)) if request.body else 0,
+                "postData": RequestWrapper._transform_body(request),
                 "url": request.url
             },
             "response": {
                 "status": resp.status_code,
-                "statusText": "?",  # TODO
+                "statusText": resp.reason,
                 "httpVersion": '',
                 "cookies": [],  # TODO list(resp.cookies),
                 "headers": RequestWrapper._dict_to_array(resp.headers),
                 "content": {
                     "size": resp.headers.get('content-length') or len(resp.text),
                     "compression": 0,
-                    "mimeType": "?",  # TODO,
+                    "mimeType": resp.headers.get('content-type', '?'),
                     "text": resp.text[:10000]
                 },
                 "bodySize": len(resp.text),
@@ -72,7 +72,7 @@ class RequestWrapper:
             "cache": {},
             "timings": {
                 "send": -1,
-                "receive": -1,  # TODO
+                "receive": resp.elapsed.total_seconds() * 1000,
                 "wait": -1
             }
         })
